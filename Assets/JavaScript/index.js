5359c2bb2c0de929840a46c3c76a6561cd9e1867
SowjanyaGiddaluri <sowjanyaworkspace1@gmail.com>
2024-08-09 21:42:03 +0530

    this is my project

---
+++ Assets/JavaScript/index.js
@@ -0,0 +1,15 @@
+ScrollReveal({
+    reset: true,
+    distance: '60px',
+    duration: 2500,
+    delay: 400
+});
+ScrollReveal().reveal('.home-content', {delay: 500, origin: 'left'});
+
+ScrollReveal().reveal('.content', {delay: 600, origin: 'left'});
+ScrollReveal().reveal('.bg-about-company-image', {delay:600, origin: 'right'});
+
+ScrollReveal().reveal('.home-card', {delay:500, origin: 'left'});
+ScrollReveal().reveal('.bg-stroy-heading', {delay:600, origin: 'left'});
+ScrollReveal().reveal('.bg-stroy-paragraph', {delay:700, origin: 'left'});
+
